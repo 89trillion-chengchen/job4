bd0002d45c5af91f7e1016d0533c872bd71752a4
chengchen-133 <1335640193@qq.com>
2021-08-05 12:55:10 +0800

    peoject4

--- classes/unitTest/GiftCodeServiceTest.php
+++ classes/unitTest/GiftCodeServiceTest.php
@@ -2,25 +2,20 @@
 
 namespace service;
 
-use framework\mvc\view\JSONView;
 use framework\util\Singleton;
-use service\GiftCodeService;
 use PHPUnit\Framework\TestCase;
-
 
 class GiftCodeServiceTest extends TestCase
 {
 
-    public function testCreatGiftCode()
+    public function testUseCode()
     {
-
         $uid='1';
         $role=1;
         $code='code_O0afeWfR';
 
         /** @var GiftCodeService $giftCodeService */
         $giftCodeService = Singleton::get(GiftCodeService::class);
-        echo new JSONView($giftCodeService->useCode($uid,$role,$code));
-
+        echo new \framework\mvc\view\JSONView($giftCodeService->useCode($uid,$role,$code));
     }
 }
